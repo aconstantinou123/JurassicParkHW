48729c52dd089cbb415516fbaf52ec40add89c6b
aconstantinou123 <a_constantinou@hotmail.co.uk>
2018-01-23 17:55:04 +0000

    can add and remove dinosaurs (by type)

--- park.js
+++ park.js
@@ -2,4 +2,12 @@ const Park = function(){
   this.enclosure = [];
 };
 
+Park.prototype.addDinosaur = function(dinosaur){
+  this.enclosure.push(dinosaur);
+}
+
+Park.prototype.removeByType = function(type){
+  this. enclosure = this.enclosure.filter(dinosaur => dinosaur.type !== type);
+}
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,2 +1,41 @@
 const assert = require('assert');
 const Dinosaur = require('../dinosaur');
+const Park = require('../park');
+
+describe('Park', function(){
+
+  let park;
+  let dinosaur1;
+  let dinosaur2;
+  let dinosaur3;
+  let dinosaur4;
+
+  beforeEach(function(){
+    park = new Park();
+    dinosaur1 = new Dinosaur("T-Rex", 2);
+    dinosaur2 = new Dinosaur("Velociraptor", 4);
+    dinosaur3 = new Dinosaur("Triceratops", 1);
+    dinosaur4 = new Dinosaur("Triceratops", 1);
+  })
+
+  it('park is empty', function(){
+    assert.strictEqual(park.enclosure.length, 0);
+  })
+
+  it('can add dinosaur to park', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    assert.strictEqual(park.enclosure.length, 3);
+  })
+
+  it('can remove dinosaurs by type', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.removeByType("Triceratops");
+    assert.strictEqual(park.enclosure.length, 2);
+  })
+
+})
